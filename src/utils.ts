98c427976abeab6c2d6f405bec2a3d070f582dc7
Gabriel Espinhara Da Silva <gabrielespinhara13@gmail.com>
2022-05-14 21:41:11 -0300

    replace language

--- src/utils.ts
+++ src/utils.ts
@@ -4,25 +4,25 @@ export default class Utils {
             return false;
         }
         strCPF = strCPF.replace(/[^\d,]/g,'')
-        var Soma;
-        var Resto;
+        var Sum;
+        var Rest;
         var i;
 
-        Soma = 0;
+        Sum = 0;
         if (strCPF === "00000000000") return false;
 
-        for (i = 1; i <= 9; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
-        Resto = (Soma * 10) % 11;
+        for (i = 1; i <= 9; i++) Sum = Sum + parseInt(strCPF.substring(i - 1, i)) * (11 - i);
+        Rest = (Sum * 10) % 11;
 
-        if ((Resto === 10) || (Resto === 11)) Resto = 0;
-        if (Resto !== parseInt(strCPF.substring(9, 10))) return false;
+        if ((Rest === 10) || (Rest === 11)) Rest = 0;
+        if (Rest !== parseInt(strCPF.substring(9, 10))) return false;
 
-        Soma = 0;
-        for (i = 1; i <= 10; i++) Soma = Soma + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
-        Resto = (Soma * 10) % 11;
+        Sum = 0;
+        for (i = 1; i <= 10; i++) Sum = Sum + parseInt(strCPF.substring(i - 1, i)) * (12 - i);
+        Rest = (Sum * 10) % 11;
 
-        if ((Resto === 10) || (Resto === 11)) Resto = 0;
-        if (Resto !== parseInt(strCPF.substring(10, 11))) return false;
+        if ((Rest === 10) || (Rest === 11)) Rest = 0;
+        if (Rest !== parseInt(strCPF.substring(10, 11))) return false;
         return true;
     }
 }
